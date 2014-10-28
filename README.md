ea86e69be5144cca90ff7b0000d2371413bf390e
Thomas Jansen <t.jansen@sap.com>
2014-10-28 09:33:22 +0100

    yolo

---
+++ README.md
@@ -0,0 +1,3 @@
+- npm install
+- download atom-shell from https://github.com/atom/atom-shell/releases into atom folder
+- start atom with current directory
---
+++ main.js
@@ -0,0 +1,28 @@
+var app = require('app');  
+var BrowserWindow = require('browser-window'); 
+
+var mainWindow = null;
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {  
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
+  var socket = require('socket.io-client')('http://localhost:3000');
+  var ipc = require('ipc');
+
+  socket.on('carst', function (data) {    
+    mainWindow.loadUrl(data.url);
+  });
+
+  socket.on('handshake', function (id, fn) {
+    fn('thomas');
+  });
+
+});
