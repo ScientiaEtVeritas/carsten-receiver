e2c10f8c0066191d1aeb68d0456a5be22c44041e
Fabian Retkowski <fabian.retkowski@sap.com>
2014-12-15 12:21:13 +0100

    path change

--- app/plugins/apps.js
+++ app/plugins/apps.js
@@ -1,21 +1,21 @@
 this.expressions = [
     {
-        expression : new RegExp("^app://index$"),
+        expression : new RegExp("^:index$"),
         fn : function(result) {
             result.window.loadUrl(result.carstenUrl + '/apps/index/index.html?channel=' + result.channel.substr(1));
         }
     },
     {
-        expression : new RegExp("^app://github$"),
+        expression : new RegExp("^:github$"),
         fn : function(result) {
-            result.window.loadUrl(result.carstenUrl + '/apps/github/index.html');
+            result.window.loadUrl(result.carstenUrl + '/apps/github/index.html?channel=' + result.channel.substr(1));
         }
     },
     {
-        expression: new RegExp("^app://9gag$"),
+        expression: new RegExp("^:9gag$"),
         fn: function (result) {
             result.window.loadUrl(result.carstenUrl + '/apps/9gag/index.html');
         }
-    },
+    }
 
 ];
